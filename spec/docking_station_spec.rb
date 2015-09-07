2539f846a20f1230019d46315fd875964de38d8d
Elin <elinnet@gmail.com>
2015-09-07 15:18:20 +0100

    dock releases a bike, reports error if no bike present

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,17 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases a working bike" do
+    subject.dock Bike.new
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  describe "#release_bike" do
+    it "raises an error when there are not bikes available" do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
 end
