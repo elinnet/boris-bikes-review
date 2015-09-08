0af5b90907d681f56fa6129345e6cca959b3064f
Elin <elinnet@gmail.com>
2015-09-08 10:54:55 +0100

    refactoring of tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,23 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
 
-  it "releases a working bike" do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
-  it 'has a default capacity' do
-    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-  end
-
   it 'shall not release a broken bike' do
-    bike = Bike.new
-    bike.report_broken
+    bike = double :bike, working?: false
     subject.dock bike
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
@@ -28,19 +16,30 @@ describe DockingStation do
     end
   end
 
-  describe "station capacity" do
-    it "can adjusted for a larger number" do
+  describe "#dock" do
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it "releases a working bike" do
+      bike = double :bike, working?: true
+      subject.dock bike
+      expect(subject.release_bike).to be_working
+    end
+
+    it 'has a default capacity' do
+      expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+    end
+
+    it "raises an error when full" do
+      subject.capacity.times { subject.dock double :bike }
+      expect { subject.dock double :bike }.to raise_error 'Docking station is full'
+    end
+
+    it "station capactiy can adjusted for a larger number" do
       subject.capacity = 50
       expect(subject.capacity).to eq 50
     end
-  end
 
-  describe "#dock" do
-    it "raises an error when full" do
-      subject.capacity.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station is full'
-    end
   end
-
 
 end
