b8440523eb5a1f44688524c5b272748569b9c542
Elin <elinnet@gmail.com>
2015-09-08 22:28:30 +0100

    bike_container module linked in with van

--- lib/van.rb
+++ lib/van.rb
@@ -1,24 +1,19 @@
-
+require_relative 'bike_container'
 
 class Van
 
-  DEFAULT_CAPACITY = 25
-
-  attr_reader :capacity
-
-  def initialize
-    @capacity = DEFAULT_CAPACITY
-    @bikes = []
-  end
+  include BikeContainer
 
   def take_bike bike
-    fail "The van is full" if @bikes.count >= 25
+    fail "The van is full" if @bikes.count >= @capacity
     @bikes << bike
   end
+  alias :add_bike :take_bike
 
   def unload_bike bike
     fail "There are no bikes in the van" if @bikes.empty?
     @bikes.pop
   end
+  alias :remove_bike :unload_bike
 
 end
